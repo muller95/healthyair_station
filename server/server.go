8bfe8c977bfd214eb708bc27d373b120dd62ef18
muller95 <muller95@yandex.ru>
2017-04-07 07:57:52 +0200

    Added POST request in Go, need to add datetime

--- server/server.go
+++ server/server.go
@@ -4,11 +4,18 @@ import (
 	"bytes"
 	"os/exec"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"strings"
 )
 
 var serverErrors map[string]string
+var domainName = "http://healthyair.ru/data_listener.php"
+var userEmail = "muller95@yandex.ru"
+var userPassword = "Warcraft123Ab"
+var stationName = "station"
 
 func main() {
 	var out bytes.Buffer
@@ -39,4 +46,27 @@ func main() {
 	}
 
 	fmt.Println(outputCO2, outputSHT)
+
+	if outputCO2[0] == "OK" && outputSHT[0] == "OK" {
+		shtMeasures := strings.Split(outputSHT[1], " ")
+		fmt.Println(shtMeasures)
+		reqData := url.Values{"email": {userEmail}, "passwd": {userPassword},
+			"stname": {stationName}, "t": {shtMeasures[0]}, "rh": {shtMeasures[1]},
+			"co2": {outputCO2[1]}}
+		reqBody := bytes.NewBufferString(reqData.Encode())
+		fmt.Println(reqData.Encode())
+		resp, err := http.Post(domainName, "application/x-www-form-urlencoded", reqBody)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if err == nil {
+			defer resp.Body.Close()
+			respData, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println(string(respData[:]))
+		}
+	}
 }
